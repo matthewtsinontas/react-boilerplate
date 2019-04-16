e201cbd46ca3c907a67b0c8b412ce5ca53ca97aa
Matthew Tsinontas <matthewtsinontas@gmail.com>
2019-04-16 16:42:22 +0100

    Testing linter

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './src';
-
+sdsd;
 ReactDOM.render(<Main />, document.getElementById('app'));
