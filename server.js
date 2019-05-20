af796b100e39d27da80634cc0ec800fd499235de
Matthew Tsinontas <matthewtsinontas@gmail.com>
2019-05-20 22:55:49 +0100

    Serving the static html file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 
 app.listen(process.env.PORT || 3000);
